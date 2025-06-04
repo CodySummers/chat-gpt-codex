3ab480f539c19d22b2460a7bb19887b438d653a8
CodySummers <gregory.k.wakefield@gmail.com>
2025-06-04 18:31:24 +0000

    Add page name and icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
-import './globals.css';
-import { ReactNode } from 'react';
+import { Metadata } from "next";
+import "./globals.css";
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+    title: "Steam vent",
+    description: "Spin the wheel of steam new releases to find your next game!",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
 }
