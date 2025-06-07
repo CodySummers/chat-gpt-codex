1c3f0307d8d7a29d80b0850763823e4918c0c228
CodySummers <gregory.k.wakefield@gmail.com>
2025-06-07 23:15:38 +0000

    Add loading screen

--- app/geforce-now/page.tsx
+++ app/geforce-now/page.tsx
@@ -3,19 +3,26 @@
 import { useEffect, useState } from "react";
 import Wheel from "../components/Wheel";
 import { fetchAllGames, Game } from "../lib/geforce";
+import Loading from "../loading";
 
 const characterLimit = 14;
 
 export default function GeforceNowPage() {
     const [games, setGames] = useState<Game[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         fetchAllGames()
             .then((items) => setGames(items))
-            .catch((err) => console.error(err));
+            .catch((err) => console.error(err))
+            .finally(() => setIsLoading(false));
     }, []);
 
     const hideText = games.length > 100;
+
+    if (isLoading) {
+        return <Loading />;
+    }
 
     return (
         <Wheel
